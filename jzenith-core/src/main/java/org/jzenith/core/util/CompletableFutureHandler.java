46fd97cb41e22881f8c70e2c390e5fb560e08fbf
Marcus Thiesen <marcus@thiesen.org>
2018-10-19 22:01:05 +0200

    Add test for CompleableFutureObserver

--- jzenith-core/src/main/java/org/jzenith/core/util/CompletableFutureHandler.java
+++ jzenith-core/src/main/java/org/jzenith/core/util/CompletableFutureHandler.java
@@ -32,5 +32,4 @@ public class CompletableFutureHandler<T> extends CompletableFuture<T> {
         };
     }
 
-
 }
---
+++ jzenith-core/src/test/java/org/jzenith/core/util/CompletableFutureObserverTest.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright © 2018 Marcus Thiesen (marcus@thiesen.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jzenith.core.util;
+
+import io.reactivex.Completable;
+import org.junit.jupiter.api.Test;
+import org.jzenith.core.JZenithException;
+
+import java.util.concurrent.ExecutionException;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CompletableFutureObserverTest {
+
+    @Test
+    public void testSuccess() throws ExecutionException, InterruptedException {
+        final CompletableFutureObserver observer = new CompletableFutureObserver();
+
+        Completable.complete().subscribe(observer.observer());
+
+        observer.get();
+    }
+
+    @Test
+    public void testError() {
+        final CompletableFutureObserver observer = new CompletableFutureObserver();
+
+        Completable.fromCallable(() -> {
+            throw new JZenithException("fail");
+        }).subscribe(observer.observer());
+
+        assertThrows(ExecutionException.class, () -> observer.get());
+    }
+
+}
